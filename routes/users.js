98718befe0192b47204ae1a10ecce9349341a86e
FabrizioSG <fabriziosg95@gmail.com>
2023-03-22 11:12:52 -0600

    Logica de razones

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/users.controller");
 const transactionController = require("../controllers/transactions.controller");
 const planDeudaController = require("../controllers/planDeuda.controller");
+const razonesController = require("../controllers/razones.controller");
 
 router.get('/', userController.getUsers)
 router.post('/', userController.signUp)
@@ -16,5 +17,8 @@ router.post('/transactions', transactionController.createTransaction);
 
 router.get('/deudas/:id', planDeudaController.getPlanDeudasFromUser);
 router.post('/deudas', planDeudaController.createPlanDeuda);
+
+router.get('razones/:id', razonesController.getRazonessFromUser);
+router.post('/razones', razonesController.createRazones);
 
 module.exports = router;
